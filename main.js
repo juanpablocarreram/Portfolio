1e374df8825257f382f603f0f0e91e1743618c2d
Juan Pablo Carrera <140585461+juanpablocarreram@users.noreply.github.com>
2024-03-07 22:31:37 -0600

    Feat(style):add animation in certain pixels two mainsectiondownpanel headers
    Add scroll event listener for create the Y axis scroll position and
    style the elements backgorundPositionY and appear animations when view.

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const paraboll = document.querySelector(".mainSection");
 document.addEventListener("scroll",() => {
 const scrolled = window.scrollY;
+const mainSectionTwoPanelDownTitleOne = document.querySelector("#mainSectionTwoPanelDownTitleOne");
+const mainSectionTwoPanelDownTitleTwo = document.querySelector("#mainSectionTwoPanelDownTitleTwo");
+const fixed = document.querySelector(".fixed");
 paraboll.style.backgroundPositionY= scrolled * .5 + "px";
+if(scrolled>=1250 && scrolled<=2200){
+   
+    mainSectionTwoPanelDownTitleOne.style.animation="entering-left ease-in-out .4s forwards"
+    mainSectionTwoPanelDownTitleTwo.style.animation="entering-right ease-in-out .4s forwards"
+}
+else{
+   
+}
 })
+
